832d5e9afa966a5a0db3d9e6f9e2066d29c1b6e7
Mikhail <wowivan2011@yandex.ru>
2020-11-06 20:19:10 +0300

    Initial Commit

---
+++ MotiWade/MotiWadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiWadeApp.swift
+//  MotiWade
+//
+//  Created by Mikhail Ivanov on 06.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MotiWadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
